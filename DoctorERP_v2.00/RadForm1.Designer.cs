bfaec10767d9bd49af89a542de5ada2582d043ca
Aly Mohamed <aly.mohamed@ahram.org.eg>
2024-10-16 21:19:37 +0300

    Add project files.

---
+++ DoctorERP_v2.00/RadForm1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DoctorERP_v2_00
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Telerik.WinControls.UI.CartesianArea cartesianArea2 = new Telerik.WinControls.UI.CartesianArea();
+            this.radSchedulerNavigator1 = new Telerik.WinControls.UI.RadSchedulerNavigator();
+            this.radChartView1 = new Telerik.WinControls.UI.RadChartView();
+            this.materialTheme1 = new Telerik.WinControls.Themes.MaterialTheme();
+            this.materialBlueGreyTheme1 = new Telerik.WinControls.Themes.MaterialBlueGreyTheme();
+            this.materialPinkTheme1 = new Telerik.WinControls.Themes.MaterialPinkTheme();
+            this.materialTealTheme1 = new Telerik.WinControls.Themes.MaterialTealTheme();
+            ((System.ComponentModel.ISupportInitialize)(this.radSchedulerNavigator1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radChartView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radSchedulerNavigator1
+            // 
+            this.radSchedulerNavigator1.AssociatedScheduler = null;
+            this.radSchedulerNavigator1.DateFormat = "yyyy/MM/dd";
+            this.radSchedulerNavigator1.Location = new System.Drawing.Point(288, 90);
+            this.radSchedulerNavigator1.Name = "radSchedulerNavigator1";
+            this.radSchedulerNavigator1.NavigationStepType = Telerik.WinControls.UI.NavigationStepTypes.Day;
+            // 
+            // 
+            // 
+            this.radSchedulerNavigator1.RootElement.StretchVertically = false;
+            this.radSchedulerNavigator1.Size = new System.Drawing.Size(472, 78);
+            this.radSchedulerNavigator1.TabIndex = 0;
+            // 
+            // radChartView1
+            // 
+            this.radChartView1.AreaDesign = cartesianArea2;
+            this.radChartView1.Location = new System.Drawing.Point(393, 224);
+            this.radChartView1.Name = "radChartView1";
+            this.radChartView1.ShowGrid = false;
+            this.radChartView1.Size = new System.Drawing.Size(480, 320);
+            this.radChartView1.TabIndex = 1;
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 562);
+            this.Controls.Add(this.radChartView1);
+            this.Controls.Add(this.radSchedulerNavigator1);
+            this.Name = "RadForm1";
+            this.Text = "RadForm1";
+            this.Load += new System.EventHandler(this.RadForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radSchedulerNavigator1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radChartView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadSchedulerNavigator radSchedulerNavigator1;
+        private Telerik.WinControls.UI.RadChartView radChartView1;
+        private Telerik.WinControls.Themes.MaterialTheme materialTheme1;
+        private Telerik.WinControls.Themes.MaterialBlueGreyTheme materialBlueGreyTheme1;
+        private Telerik.WinControls.Themes.MaterialPinkTheme materialPinkTheme1;
+        private Telerik.WinControls.Themes.MaterialTealTheme materialTealTheme1;
+    }
+}
